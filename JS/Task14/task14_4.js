67e924a7e751c6d7058d057722d394d1699afdc1
AleX <80423508+xxxAlexander@users.noreply.github.com>
2023-10-03 15:25:32 +0300

    task14_4.js

--- JS/Task14/task14_4.js
+++ JS/Task14/task14_4.js
@@ -1,3 +1,8 @@
+//Дано: Функція приймає Об’єкт типу {[name]: {age: number, city: string}} 
+//Результат: Вивести у консоль масив із іменами людей які із міста "London" та старше 18 років 
+
+
+
 function findUser(initialObject) {
     return Object.keys(initialObject).filter(name => initialObject[name].age > 18 && initialObject[name].city === "London");
 }
