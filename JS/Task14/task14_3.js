5c7605ddebc264d16b6e5370c6bf87162bfda1f9
AleX <80423508+xxxAlexander@users.noreply.github.com>
2023-10-03 15:25:10 +0300

    task14_3.js

--- JS/Task14/task14_3.js
+++ JS/Task14/task14_3.js
@@ -1,3 +1,7 @@
+//Дано: функція яка приймає масив елементів будь-яких типів 
+//Результат: вивести у консоль масив який містить лише стрічки початкового масиву 
+
+
 function filterArray(initialArray) {
     console.log(initialArray.filter((item)=> typeof item === 'string' ))
 }
