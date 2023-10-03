820018a4edc63f833bff57131b4eab6f66934d3e
AleX <80423508+xxxAlexander@users.noreply.github.com>
2023-10-03 15:27:21 +0300

    task10_2.js

--- JS/Task10/task10_2.js
+++ JS/Task10/task10_2.js
@@ -1,9 +1,8 @@
 //Дано: масив [3, 2, "2", null, 1.5, 9.5, undefined];
 
 //Результат: Вивести у консоль суму чисел у масиві.
-
+const initialArray = [3, 2, "2", null, 1.5, 9.5, undefined];
 function calculateSumOfArray() {
-    const initialArray = [3, 2, "2", null, 1.5, 9.5, undefined];
     let sum = 0;
 
     for (let i = 0; i < initialArray.length; i++) {
